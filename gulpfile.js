a8ec7fd65f3f6bcaf97229233ec19b88e3844925
Ludikyiv <ludiky30@gmail.com>
2020-03-09 03:22:22 +0700

    adding css minification to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ gulp.task('hello', (done) => {
   done();
 });
 
+//Для автоматического обновления страницы в браузере
 const browserSync = require('browser-sync').create();
 
-// Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -16,3 +16,16 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+//для минимализации css
+let cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+ 
+gulp.task('minify-css', function (done) {
+    gulp.src('./css/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./css-dist'));
+    gulp.watch("./css/*.css", gulp.parallel('minify-css'));
+});
+
