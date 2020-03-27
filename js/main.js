9065d83eb37bcfb03e7a449abbee437cdcafdba3
Ludikyiv <ludiky30@gmail.com>
2020-03-27 16:59:32 +0700

    refactor: redid the modal window on jquery. feat: add button up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+/*document.addEventListener("DOMContentLoaded", (event) => {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -26,5 +26,46 @@ document.addEventListener("DOMContentLoaded", (event) => {
   })
 
 
+
+}); */
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close'),
+      btn = $('.button--up');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  modal.on('click', function (event) {
+    if (event.target.classList.contains('modal'))
+      {
+        modal.toggleClass('modal--visible');
+      } 
+  });
+
+  $(document).on('keydown', function (event) {
+    if (event.code == 'Escape' && modal.hasClass('modal--visible')) {
+      modal.toggleClass('modal--visible');
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.removeClass('button--hidden');
+    } else {
+      btn.addClass('button--hidden');
+    }
+  });
+
+  btn.on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
 
 });
